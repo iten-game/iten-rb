4dd5087a7182d5cfa427d3b6e7a233e41d9b26fa
gavin <gavin@vm2.tau.uk.com>
2016-01-11 12:59:08 +0000

    make this XSS-safe

--- inc/RB/ErrorHandler.php
+++ inc/RB/ErrorHandler.php
@@ -3,9 +3,14 @@
 class RB_ErrorHandler {
 
 	static function error($errno, $errstr, $file, $line, $context) {
-		print "<pre>Error: {$errstr} at {$file} line {$line}";
+		printf(
+			'<pre>Error: %s at %s line %u\n',
+			htmlspecialchars(errstr),
+			htmlspecialchars($file),
+			$line
+		);
 		debug_print_backtrace();
-		print "</pre>";
+		print '</pre>';
 		if ($errno == E_USER_ERROR) exit(1);
 	}
 }
